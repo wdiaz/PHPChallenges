6617a0d0a9619a2a7f91b3f2ebc3b944daae40f9
Walter Diaz <wdiaz503@gmail.com>
2017-09-20 04:48:59 +0000

    first commit

---
+++ recursive_palindrome.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Write a function that determines if a string is a palindrome
+ * Recursive Solution Only
+ */
+function isPalindrome($str) {
+    if(strlen($str) <= 1) return true;
+    $first = $str[0];
+    $last = substr($str, -1, 1);
+    return $first == $last && isPalindrome(substr($str,1, strlen($str)-2));
+}
+
+printf("racecar: %s", isPalindrome('racecar'));
